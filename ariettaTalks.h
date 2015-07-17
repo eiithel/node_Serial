c598e76d0cd4a5cfbd59ebdd3a797428ae29516c
Ethel Marquer <ethel.marquer@gmail.com>
2015-07-17 16:37:57 +0200

    Header which describes commands from Arietta

---
+++ ariettaTalks.h
@@ -0,0 +1,15 @@
+
+#ifndef AriettaTalks_h
+#define AriettaTalks_h
+
+#define MAX_COMMAND_LENGTH 255
+
+char* replyOK = {"OK\n"};
+
+char resetByte = '!';
+char stopByte = '#';
+
+char* commandLEDOn = {"LEDON"};
+char* commandLEDOff = {"LEDOFF"};
+
+#endif
