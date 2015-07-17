eb2dd9391bf1daf6bb402596f1a2ca467e050ab5
Ethel Marquer <ethel.marquer@gmail.com>
2015-07-17 17:01:29 +0200

    choice of sttyS2 for serial communication

--- teensyLed.c
+++ teensyLed.c
@@ -1,5 +1,9 @@
 #include "ariettaTalks.h"
 
+#include "test_HID.h"
+#include <Arduino.h>
+#include "Wire.h"
+
 int incomingByte = 0;
 int charCount = 0;
 char command[MAX_COMMAND_LENGTH + 1]; // leave space for \0
@@ -8,12 +12,12 @@ int led = 13;
 
 void setup() {
   pinMode(led, OUTPUT);
-  Serial.begin(9600);
+  Serial2.begin(9600);
 }
 
 void loop() {
-  if (Serial.available()) {
-    incomingByte = Serial.read();
+  if (Serial2.available()) {
+    incomingByte = Serial2.read();
 
     if ((char)incomingByte == resetByte) {
       locked = false;
@@ -22,13 +26,13 @@ void loop() {
     }
 
     if (locked) {
-      Serial.println("@");
+      Serial2.println("@");
       return;
     }
 
     if ((char)incomingByte != stopByte) {
       if (charCount > MAX_COMMAND_LENGTH - 1) {
-        Serial.println("@");
+        Serial2.println("@");
         locked = true;
         return;
       }
@@ -42,11 +46,11 @@ void loop() {
       charCount = 0;
       if (strcmp(commandLEDOn, command) == 0) {
         digitalWrite(led, HIGH);
-        Serial.println(replyOK);
+        Serial2.println("replyOK");
       }
       else if (strcmp(commandLEDOff, command) == 0) {
         digitalWrite(led, LOW);
-        Serial.println(replyOK);
+        Serial2.println("replyOK");
       }
     }
   }
