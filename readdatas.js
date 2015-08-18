1e0700bddebf6c9933af2d59284eda75b4c8cdf1
eiithel <ethel.marquer@gmail.com>
2015-08-18 12:17:10 +0200

    /dev/ttyS1 instead of /dev/ttyS2

--- readdatas.js
+++ readdatas.js
@@ -1,5 +1,5 @@
 var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("/dev/ttyS2");
+var serialport = new SerialPort("/dev/ttyS1");
 serialport.on('open', function(){
   console.log('Serial Port Open');
   serialport.on('data', function(data){
